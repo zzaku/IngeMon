44b9e6aaa9f786c45b3329b3b86b5225e1c5566a
zzaku <enzo.makked@gmail.com>
2021-11-13 21:02:51 +0100

    Chnaged form

--- src/Components/Monitoring.js
+++ src/Components/Monitoring.js
@@ -1,93 +1,148 @@
-import '../Mon-style/Monitoring.css'
-import Ram from './Ram'
-import Cpu from './Cpu'
-import Disk from './Disk'
-import Debit from './Debit'
-import Default from './Default'
-import { Bar } from "react-chartjs-2"
-import { useState } from 'react'
+import "../Mon-style/Monitoring.css";
+import Ram from "./Ram";
+import Cpu from "./Cpu";
+import Disk from "./Disk";
+import Debit from "./Debit";
+import Default from "./Default";
+import { Bar } from "react-chartjs-2";
+import { useState } from "react";
 
+function Monitoring({
+  dataArray,
+  setDataArray,
+  dataArray1,
+  setDataArray1,
+  dataArray2,
+  setDataArray2,
+  btnChild,
+  postChild,
+  setPostChild,
+}) {
+  const [shopVisible, setShopVisible] = useState(false);
+  const [budget, setBudget] = useState();
+  const [displayButton, setDispalyButton] = useState(false);
 
-function Monitoring({dataArray, setDataArray, dataArray1, setDataArray1, dataArray2, setDataArray2, btnChild, postChild, setPostChild}){
+  const noneDefault = (
+    <div className="none-container">
+      <h3 className="none-default">
+        Ici vous sera affiché un graphique de vos performances
+      </h3>
+    </div>
+  );
 
-    const [shopVisible, setShopVisible] = useState(false)
-    const [budget, setBudget] = useState()
-    const [displayButton, setDispalyButton] = useState(false)
-    
-    const noneDefault = 
-        <div className="none-container">
-            <h3 className="none-default">
-                Ici vous sera affiché un graphique de vos performances
-            </h3>
-        </div>
+  const graphCpu = (
+    <div className="stats">
+      <div className="container_graph">
+        <Bar data={dataArray2} />
+      </div>
 
-    const graphCpu =
-        <div className="stats">
-            <div className="container_graph">
-                <Bar data={dataArray2} />
-            </div>
+      <div className="container_graph">
+        <Bar data={dataArray1} />
+      </div>
 
-            <div className="container_graph">
-                <Bar data={dataArray1} />
-            </div>
+      <div className="container_graph2">
+        <Bar data={dataArray} width={100} height={26} />
+      </div>
+    </div>
+  );
 
-            <div className="container_graph2">
-            <Bar data={dataArray} width={100} height={26} />
-            </div>
-        </div>
+  const graphRam = (
+    <div className="stats">
+      <div className="container_graph3">
+        <Bar data={dataArray} />
+      </div>
+    </div>
+  );
 
-    const graphRam = 
-        <div className="stats">
-            <div className="container_graph3">
-            <Bar data={dataArray} />
-            </div>    
-        </div>
+  const graphDisk = (
+    <div className="statsdisk">
+      <div className="container_graphdisk">
+        <Bar data={dataArray} height={300} />
+      </div>
 
-    const graphDisk = 
-        <div className="statsdisk">
-            <div className="container_graphdisk">
-            <Bar data={dataArray} height={300} />
-            </div>
+      <div className="container_graphdisk">
+        <Bar data={dataArray1} height={300} />
+      </div>
+    </div>
+  );
 
-            <div className="container_graphdisk">
-            <Bar data={dataArray1} height={300} />
-            </div>
-        </div>
+  const graphDebit = (
+    <div className="stats">
+      <div className="container_graph2">
+        <Bar data={dataArray} />
+      </div>
+    </div>
+  );
 
-    const graphDebit = 
-        <div className="stats">
-            <div className="container_graph2">
-            <Bar data={dataArray} />
-            </div>
-        </div>
-        
-    function handleChangeBudget(e){
-            setBudget(e.target.value)
-        }
+  function handleChangeBudget(e) {
+    setBudget(e.target.value);
+  }
 
-    function submitBudget(e){
-        
-    }
-        console.log(budget)
-        console.log(displayButton)
+  function submitBudget(e) {}
+  console.log(budget);
+  console.log(displayButton);
 
-    return(
-        <div className="container-monitoring">
-                <div className="analyse-performance">
-                    
-                    {btnChild === 'DEFAULT' && <Default className="clear-default" postChild={postChild} btnChild={btnChild} setDataArray={setDataArray} setDataArray1={setDataArray1} setDataArray2={setDataArray2} />}
-                    {btnChild === 'RAM' && <Ram className="data-ram" postChild={postChild} btnChild={btnChild} setDataArray={setDataArray} setDataArray1={setDataArray1} setDataArray2={setDataArray2} />}
-                    {btnChild === 'CPU' && <Cpu className="data-cpu" postChild={postChild} btnChild={btnChild} setDataArray={setDataArray} setDataArray1={setDataArray1} setDataArray2={setDataArray2} />}
-                    {btnChild === 'DISK' && <Disk className="data-disk" postChild={postChild} btnChild={btnChild} setDataArray={setDataArray} setDataArray1={setDataArray1} setDataArray2={setDataArray2} />}
-                    {btnChild === 'DEBIT' && <Debit className="data-debit" postChild={postChild} btnChild={btnChild} setDataArray={setDataArray} setDataArray1={setDataArray1} setDataArray2={setDataArray2} />}
-                </div>
+  return (
+    <div className="container-monitoring">
+      <div className="analyse-performance">
+        {btnChild === "DEFAULT" && (
+          <Default
+            className="clear-default"
+            postChild={postChild}
+            btnChild={btnChild}
+            setDataArray={setDataArray}
+            setDataArray1={setDataArray1}
+            setDataArray2={setDataArray2}
+          />
+        )}
+        {btnChild === "RAM" && (
+          <Ram
+            className="data-ram"
+            postChild={postChild}
+            btnChild={btnChild}
+            setDataArray={setDataArray}
+            setDataArray1={setDataArray1}
+            setDataArray2={setDataArray2}
+          />
+        )}
+        {btnChild === "CPU" && (
+          <Cpu
+            className="data-cpu"
+            postChild={postChild}
+            btnChild={btnChild}
+            setDataArray={setDataArray}
+            setDataArray1={setDataArray1}
+            setDataArray2={setDataArray2}
+          />
+        )}
+        {btnChild === "DISK" && (
+          <Disk
+            className="data-disk"
+            postChild={postChild}
+            btnChild={btnChild}
+            setDataArray={setDataArray}
+            setDataArray1={setDataArray1}
+            setDataArray2={setDataArray2}
+          />
+        )}
+        {btnChild === "DEBIT" && (
+          <Debit
+            className="data-debit"
+            postChild={postChild}
+            btnChild={btnChild}
+            setDataArray={setDataArray}
+            setDataArray1={setDataArray1}
+            setDataArray2={setDataArray2}
+          />
+        )}
+      </div>
 
-                {btnChild === 'DEFAULT' &&  noneDefault}
-                {btnChild === 'CPU' &&  graphCpu}
-                {btnChild === 'RAM' &&  graphRam}
-                {btnChild === 'DISK' &&  graphDisk}
-                {btnChild === 'DEBIT' &&  graphDebit}
-        </div>
-    )
+      {btnChild === "DEFAULT" && noneDefault}
+      {btnChild === "CPU" && graphCpu}
+      {btnChild === "RAM" && graphRam}
+      {btnChild === "DISK" && graphDisk}
+      {btnChild === "DEBIT" && graphDebit}
+    </div>
+  );
 }
-export default Monitoring
+export default Monitoring;
--- src/Routes/Test.js
+++ src/Routes/Test.js
@@ -1,81 +1,133 @@
-import '../Mon-style/Test.css'
+import "../Mon-style/Test.css";
 import Button from "@material-ui/core/Button";
-import Monitoring from '../Components/Monitoring';
-import { perfComponents } from '../data/data';
-import { useState, useEffect } from 'react';
+import Monitoring from "../Components/Monitoring";
+import { perfComponents } from "../data/data";
+import { useState, useEffect } from "react";
 
+function Test() {
+  const [dataArray, setDataArray] = useState({
+    labels: ["", "", ""],
+    datasets: [
+      {
+        label: "",
+        data: [],
+      },
+    ],
+  });
 
-function Test(){
-
-    const [dataArray, setDataArray] = useState({
-        labels: ["", "", ""],
-        datasets: [{
-                label: "",
-                data: []
-        }]
-})
-
-const [dataArray1, setDataArray1] = useState({
+  const [dataArray1, setDataArray1] = useState({
     labels: ["RAM utilisé", "Disponible", "Mise en cache"],
-    datasets: [{
-            label: "STATS RAM",
-            data: []
-    }]
-})
+    datasets: [
+      {
+        label: "STATS RAM",
+        data: [],
+      },
+    ],
+  });
 
-const [dataArray2, setDataArray2] = useState({
+  const [dataArray2, setDataArray2] = useState({
     labels: ["RAM utilisé", "Disponible", "Mise en cache"],
-    datasets: [{
-            label: "STATS RAM",
-            data: []
-    }]
-})
-    const [post, setPost] = useState([])
-    const [btn, setBtn] = useState('DEFAULT')
-    const [modifyPost, setModifyPost] = useState({perfComponents})
+    datasets: [
+      {
+        label: "STATS RAM",
+        data: [],
+      },
+    ],
+  });
+  const [post, setPost] = useState([]);
+  const [btn, setBtn] = useState("DEFAULT");
+  const [modifyPost, setModifyPost] = useState({ perfComponents });
 
-    function getPerf(){
-        fetch('http://localhost:4000/performances')
-        .then((response) => { 
-            return response.json()
-        })
-        .then(data => {
-            setPost([{api: data}])
-        })
-    }
+  function getPerf() {
+    fetch("http://localhost:4000/performances")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setPost([{ api: data }]);
+      });
+  }
 
-    useEffect(() => {
-        const requestOptions = {
-                    method: 'PATCH',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(perfComponents)
-        };
+  useEffect(() => {
+    const requestOptions = {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(perfComponents),
+    };
 
-        fetch('http://localhost:4000/performances/60fe7660c1bdfd3cf80dd8bb', requestOptions)
-    }, []);
+    fetch(
+      "http://localhost:4000/performances/60fe7660c1bdfd3cf80dd8bb",
+      requestOptions
+    );
+  }, []);
 
-    function clear(){
-        setBtn('DEFAULT')
-        setDataArray({})
-        setDataArray1({})
-        setDataArray2({})
-    }
+  function clear() {
+    setBtn("DEFAULT");
+    setDataArray({});
+    setDataArray1({});
+    setDataArray2({});
+  }
 
-    return(
-        <div className="container-performances">
-            <h2>
-                Testez les performances de vos composants !
-            </h2>
-            <ul className="BarNav-test">
-                <li className="BarNav-composant"><Button variant="contained" color="primary" onClick={()=> clear()}>Default</Button></li>
-                <li className="BarNav-composant"><Button variant="contained" color="primary" onClick={()=> getPerf() + setBtn('CPU')}>CPU</Button></li>
-                <li className="BarNav-composant"><Button variant="contained" color="primary" onClick={() => getPerf() + setBtn('RAM')}>RAM</Button></li>
-                <li className="BarNav-composant"><Button variant="contained" color="primary" onClick={() => getPerf() + setBtn('DISK')}>DISK</Button></li>
-                <li className="BarNav-composant"><Button variant="contained" color="primary" onClick={() => getPerf() + setBtn('DEBIT')}>DEBIT</Button></li>
-            </ul>
-                <Monitoring postChild={post} setPostChild={setPost} btnChild={btn} setBtnChild={setBtn} dataArray={dataArray} setDataArray={setDataArray} dataArray1={dataArray1} setDataArray1={setDataArray1} dataArray2={dataArray2} setDataArray2={setDataArray2} />
-        </div>
-    )
+  return (
+    <div className="container-performances">
+      <h2>Testez les performances de vos composants !</h2>
+      <ul className="BarNav-test">
+        <li className="BarNav-composant">
+          <Button variant="contained" color="primary" onClick={() => clear()}>
+            Default
+          </Button>
+        </li>
+        <li className="BarNav-composant">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => getPerf() + setBtn("CPU")}
+          >
+            CPU
+          </Button>
+        </li>
+        <li className="BarNav-composant">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => getPerf() + setBtn("RAM")}
+          >
+            RAM
+          </Button>
+        </li>
+        <li className="BarNav-composant">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => getPerf() + setBtn("DISK")}
+          >
+            DISK
+          </Button>
+        </li>
+        <li className="BarNav-composant">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => getPerf() + setBtn("DEBIT")}
+          >
+            DEBIT
+          </Button>
+        </li>
+      </ul>
+      <Monitoring
+        postChild={post}
+        setPostChild={setPost}
+        btnChild={btn}
+        setBtnChild={setBtn}
+        dataArray={dataArray}
+        setDataArray={setDataArray}
+        dataArray1={dataArray1}
+        setDataArray1={setDataArray1}
+        dataArray2={dataArray2}
+        setDataArray2={setDataArray2}
+      />
+    </div>
+  );
 }
 
-export default Test
+export default Test;
